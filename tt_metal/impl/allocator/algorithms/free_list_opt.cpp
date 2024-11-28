79ccf00e7e0c7ff3643ed5a5eca73e5162dac897
marty1885 <marty188586@gmail.com>
2024-11-28 17:25:19 +0800

    update condition

--- tt_metal/impl/allocator/algorithms/free_list_opt.cpp
+++ tt_metal/impl/allocator/algorithms/free_list_opt.cpp
@@ -132,7 +132,8 @@ std::optional<DeviceAddr> FreeListOpt::allocate(DeviceAddr size_bytes, bool bott
     }
     size_t allocated_block_index = allocate_in_block(target_block_index, alloc_size, offset);
     DeviceAddr start_address = block_address_[allocated_block_index];
-    TT_FATAL(start_address + offset_bytes_ < address_limit || address_limit == 0, "Out of Memory: Cannot allocate at an address below {}. Allocation ends at {}", address_limit, start_address + offset_bytes_);
+    TT_FATAL(start_address + offset_bytes_ >= address_limit, "Out of Memory: Cannot allocate at an address below {}. Allocation at {}",
+        address_limit, start_address + offset_bytes_);
     return start_address + offset_bytes_;
 }
 
